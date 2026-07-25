67ad6baf717c63e1f9ecffa346801b6b0e970f9c
Tabish Firoz <mdtabishfiroz04@gmail.com>
2026-07-25 13:09:42 +0530

    Initial Commit

---
+++ Backend/src/main/java/com/example/demo/controller/test.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+	@RequestMapping("/")
+	public String Name() {
+		return "Tabis";
+	}
+}
---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        React + Tailwind Tabish
+      </h1>
+    </div>
+  );
+}
+
+export default App;
